bb170a4a74f5f2c695f0b4f256eaae4ad7f919e8
okivankenobi <136176790+okivankenobi@users.noreply.github.com>
2025-11-03 22:23:41 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-03T19:23:32.842Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-03T19:23:40.959Z
+Commit: Fix CSS styling bug on mobile devices
