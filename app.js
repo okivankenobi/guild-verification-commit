7971617c5a7e1b4ca81742033822a350299611db
okivankenobi <136176790+okivankenobi@users.noreply.github.com>
2025-11-03 22:23:48 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-03T19:23:45.626Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-03T19:23:48.389Z
+Commit: Fix broken links in documentation
