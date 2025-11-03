521269c77ad0cf53db18839fcae076ce22c2df63
okivankenobi <136176790+okivankenobi@users.noreply.github.com>
2025-11-03 22:53:58 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-03T19:53:56.552Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-03T19:53:58.510Z
+Commit: Fix memory leak in event handlers
