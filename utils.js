4e08b01e1d74e647188382901a3f1dc90a5e8db3
okivankenobi <136176790+okivankenobi@users.noreply.github.com>
2025-11-03 22:53:56 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-03T19:53:55.560Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-03T19:53:56.552Z
+Commit: Resolve timezone issues in date handling
